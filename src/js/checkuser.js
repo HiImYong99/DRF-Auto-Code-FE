c132c6d89c97b7123ef8a4645dc7d445bdd7a60a
yongtae.kim <good19422@naver.com>
2023-11-23 00:07:12 +0900

    ✨ Feat: 회원관련 API 연결

---
+++ src/js/checkuser.js
@@ -0,0 +1,47 @@
+// 쿠키 이름으로 쿠키의 값을 가져오는 함수
+const getCookie = cookie_name => {
+  const name = cookie_name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(";");
+
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i].trim();
+
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+
+  return "";
+};
+
+// 페이지 로드 시 실행되는 함수
+
+window.onload = () => {
+  const auth = document.querySelectorAll(".auth");
+  const notauth = document.querySelectorAll(".notauth");
+  const access = getCookie("my-app-auth");
+  const refresh = getCookie("my-refresh-token");
+
+  // access 쿠키가 존재하는 경우
+  if (access !== "") {
+    auth.forEach(element => {
+      element.classList.remove("hidden");
+    });
+    notauth.forEach(element => {
+      element.classList.add("hidden");
+    });
+  } else {
+    auth.forEach(element => {
+      element.classList.add("hidden");
+    });
+    notauth.forEach(element => {
+      element.classList.remove("hidden");
+    });
+    const request_btn = document.getElementById("request-btn");
+    request_btn.textContent = "로그인이 필요합니다.";
+    request_btn.addEventListener("click", () => {
+      window.location.href = "pages/login.html"; // 로그인 페이지 경로에 맞게 수정
+    });
+  }
+};
--- src/js/login.js
+++ src/js/login.js
@@ -18,26 +18,25 @@ const api_login = async e => {
 
   formData.append("email", email);
   formData.append("password", password);
-  try {
-    // fetch를 이용해서 서버에 POST 요청을 보낸다.
-    const response = await fetch("http://127.0.0.1:8000/accounts/login/", {
-      method: "POST",
-      headers: {},
-      body: formData,
+
+  // fetch를 이용해서 서버에 POST 요청을 보낸다.
+  await fetch("http://127.0.0.1:8000/accounts/login/", {
+    method: "POST",
+    headers: {},
+    credentials: "include",
+    body: formData,
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.non_field_errors) {
+        alert(data.non_field_errors[0]);
+      } else {
+        console.log(data);
+        location.href = "/index.html";
+      }
+    })
+    .catch(err => {
+      console.log(err);
     });
-
-    const data = await response.json();
-
-    // 응답 데이터 확인
-    if (response.ok) {
-      // 로그인 성공
-      location.href = "/index.html";
-    } else {
-      // 로그인 실패 시
-      console.error("로그인 실패:", data);
-    }
-  } catch (err) {
-    console.error("로그인 오류:", err);
-  }
 };
 login_btn.addEventListener("click", api_login);
