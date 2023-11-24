2297f54352553e6ecb020c30ee1d9d9908f32ec3
yongtae.kim <good19422@naver.com>
2023-11-24 16:43:55 +0900

    ✨ Feat: 로그아웃 서버측 요청 구현

--- src/js/logout.js
+++ src/js/logout.js
@@ -4,12 +4,17 @@ const deleteCookie = cookie_name => {
   document.cookie = `${cookie_name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 };
 
-const api_logout = () => {
-  deleteCookie("my-app-auth");
-  deleteCookie("my-refresh-token");
-  deleteCookie("csrftoken");
-  deleteCookie("sessionid");
-  location.href = "/index.html"; // Redirect to logout success page or any other page
+const api_logout = async () => {
+  await fetch("http://127.0.0.1:8000/accounts/logout/", {
+    method: "POST",
+    headers: {},
+    credentials: "include",
+  })
+    .then(res => res.json())
+    .catch(err => {
+      console.log(err);
+    });
+  location.reload();
 };
 
 $logout_btn.addEventListener("click", api_logout);
